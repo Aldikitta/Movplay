5488e78e6deb06ecce45fb694cfb9620987f32ce
Aldikitta <andi.kitta77@gmail.com>
2022-06-18 13:35:08 +0800

    dependency injection hilt

--- app/src/main/java/com/aldikitta/jetmvvmmovie/di/NetworkModule.kt
+++ app/src/main/java/com/aldikitta/jetmvvmmovie/di/NetworkModule.kt
@@ -1,4 +1,90 @@
 package com.aldikitta.jetmvvmmovie.di
 
-class NetworkModule {
+import com.aldikitta.jetmvvmmovie.data.datasource.remote.ApiService
+import com.aldikitta.jetmvvmmovie.data.datasource.remote.ApiURL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Converter
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+    /**
+     * Provides BaseUrl as string
+     */
+    @Singleton
+    @Provides
+    fun provideBaseURL(): String {
+        return ApiURL.BASE_URL
+    }
+
+    /**
+     * Provides LoggingInterceptor for api information
+     */
+    @Singleton
+    @Provides
+    fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+    }
+
+    /**
+     * Provides custom OkkHttp
+     */
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        val okHttpClient = OkHttpClient().newBuilder()
+
+        okHttpClient.callTimeout(40, TimeUnit.SECONDS)
+        okHttpClient.connectTimeout(40, TimeUnit.SECONDS)
+        okHttpClient.readTimeout(40, TimeUnit.SECONDS)
+        okHttpClient.writeTimeout(40, TimeUnit.SECONDS)
+        okHttpClient.addInterceptor(loggingInterceptor)
+        okHttpClient.build()
+        return okHttpClient.build()
+    }
+
+    /**
+     * Provides converter factory for retrofit
+     */
+    @Singleton
+    @Provides
+    fun provideConverterFactory(): Converter.Factory {
+        return GsonConverterFactory.create()
+    }
+
+    /**
+     * Provides ApiServices client for Retrofit
+     */
+    @Singleton
+    @Provides
+    fun provideRetrofitClent(
+        baseUrl: String,
+        okHttpClient: OkHttpClient,
+        converterFactory: Converter.Factory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .client(okHttpClient)
+            .addConverterFactory(converterFactory)
+            .build()
+    }
+
+    /**
+     * Provides Api Service using retrofit
+     */
+    @Singleton
+    @Provides
+    fun provideRestApiService(retrofit: Retrofit): ApiService {
+        return retrofit.create(ApiService::class.java)
+    }
+
 }
--- app/src/main/java/com/aldikitta/jetmvvmmovie/di/RepositoryModule.kt
+++ app/src/main/java/com/aldikitta/jetmvvmmovie/di/RepositoryModule.kt
@@ -1,4 +1,27 @@
 package com.aldikitta.jetmvvmmovie.di
 
-class RepositoryModule {
+import com.aldikitta.jetmvvmmovie.data.datasource.remote.ApiService
+import com.aldikitta.jetmvvmmovie.data.repository.MovieRepository
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RepositoryModule {
+    /**
+     * Provides RemoteDataRepository for access api service method
+     */
+    @Singleton
+    @Provides
+    fun provideMovieRepository(
+        apiService: ApiService,
+    ): MovieRepository {
+        return MovieRepository(
+            apiService
+        )
+    }
+
 }
